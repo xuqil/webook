e41c5af77d6b2c0d0729b81154f33b1b3c05cf6a
xuqil <xuqi19218@163.com>
2023-08-05 18:58:11 +0800

    first commit

---
+++ internal/web/user.go
@@ -0,0 +1,84 @@
+package web
+
+import (
+	regexp "github.com/dlclark/regexp2"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type UserHandler struct {
+	emailExp    *regexp.Regexp
+	passwordExp *regexp.Regexp
+}
+
+func NewUserHandler() *UserHandler {
+	const (
+		emailRegexPattern    = "^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$"
+		passwordRegexPattern = `^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$`
+	)
+	emailExp := regexp.MustCompile(emailRegexPattern, regexp.None)
+	passwordExp := regexp.MustCompile(passwordRegexPattern, regexp.None)
+	return &UserHandler{
+		emailExp:    emailExp,
+		passwordExp: passwordExp,
+	}
+}
+
+func (u *UserHandler) RegisterRoutes(server *gin.Engine) {
+	ug := server.Group("/users")
+	ug.GET("/profile", u.Profile)
+	ug.POST("/signup", u.SignUp)
+	ug.POST("/login", u.Login)
+	ug.POST("/edit", u.Edit)
+}
+
+func (u *UserHandler) SignUp(ctx *gin.Context) {
+	type SignUpReq struct {
+		Email           string `json:"email"`
+		ConfirmPassword string `json:"confirmPassword"`
+		Password        string `json:"password"`
+	}
+
+	var req SignUpReq
+	// 获取请求参数
+	if err := ctx.Bind(&req); err != nil {
+		return
+	}
+
+	ok, err := u.emailExp.MatchString(req.Email)
+	if err != nil {
+		ctx.String(http.StatusOK, "系统错误")
+		return
+	}
+	if !ok {
+		ctx.String(http.StatusOK, "你的邮箱格式不对")
+		return
+	}
+	if req.ConfirmPassword != req.Password {
+		ctx.String(http.StatusOK, "两次输入的密码不一致")
+		return
+	}
+	ok, err = u.passwordExp.MatchString(req.Password)
+	if err != nil {
+		ctx.String(http.StatusOK, "系统错误")
+		return
+	}
+	if !ok {
+		ctx.String(http.StatusOK, "密码必须大于8位，包含数字、特殊字符")
+		return
+	}
+
+	ctx.String(http.StatusOK, "注册成功")
+}
+
+func (u *UserHandler) Login(ctx *gin.Context) {
+
+}
+
+func (u *UserHandler) Edit(ctx *gin.Context) {
+
+}
+
+func (u *UserHandler) Profile(ctx *gin.Context) {
+
+}
--- main.go
+++ main.go
@@ -1,1 +1,33 @@
-package webook
+package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/xuqil/webook/internal/web"
+	"strings"
+	"time"
+)
+
+func main() {
+	server := gin.Default()
+	server.Use(cors.New(cors.Config{
+		//AllowOrigins: []string{"*"},
+		//AllowMethods: []string{"POST", "GET"},
+		AllowHeaders: []string{"Content-Type", "Authorization"},
+		//ExposeHeaders: []string{"x-jwt-token"},
+		// 是否允许你带 cookie 之类的东西
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				// 你的开发环境
+				return true
+			}
+			return strings.Contains(origin, "yourcompany.com")
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+	u := web.NewUserHandler()
+	u.RegisterRoutes(server)
+
+	server.Run(":8080")
+}
