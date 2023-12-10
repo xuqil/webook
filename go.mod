7e0180e448c0508c16fb7bcb7a5c77d5a4d48860
xuqil <xuqi19218@163.com>
2023-12-10 23:10:21 +0800

    测试

--- go.mod
+++ go.mod
@@ -9,16 +9,19 @@ require (
 	github.com/gin-gonic/gin v1.9.1
 	github.com/go-sql-driver/mysql v1.7.0
 	github.com/golang-jwt/jwt/v5 v5.1.0
+	github.com/google/wire v0.5.0
 	github.com/redis/go-redis/v9 v9.3.0
 	github.com/stretchr/testify v1.8.3
 	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common v1.0.800
 	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/sms v1.0.800
+	go.uber.org/mock v0.3.0
 	golang.org/x/crypto v0.9.0
-	gorm.io/driver/mysql v1.5.1
+	gorm.io/driver/mysql v1.5.2
 	gorm.io/gorm v1.25.3
 )
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
@@ -30,7 +33,6 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
-	github.com/google/wire v0.5.0 // indirect
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/gorilla/sessions v1.2.1 // indirect
@@ -50,6 +52,7 @@ require (
 	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/web/user_test.go
+++ internal/web/user_test.go
@@ -1,8 +1,18 @@
 package web
 
 import (
+	"bytes"
+	"errors"
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/xuqil/webook/internal/domain"
+	"github.com/xuqil/webook/internal/service"
+	svcmocks "github.com/xuqil/webook/internal/service/mocks"
+	"go.uber.org/mock/gomock"
 	"golang.org/x/crypto/bcrypt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -14,4 +24,177 @@ func TestEncrypt(t *testing.T) {
 	}
 	err = bcrypt.CompareHashAndPassword(encrypted, []byte(password))
 	assert.NoError(t, err)
+}
+
+func TestUserHandler_SignUp(t *testing.T) {
+	testCases := []struct {
+		name string
+
+		mock func(ctrl *gomock.Controller) service.UserService
+
+		reqBody string
+
+		wantCode int
+		wantBody string
+	}{
+		{
+			name: "注册成功",
+			mock: func(ctrl *gomock.Controller) service.UserService {
+				usersvc := svcmocks.NewMockUserService(ctrl)
+				usersvc.EXPECT().SignUp(gomock.Any(), domain.User{
+					Email:    "123@qq.com",
+					Password: "hello#world123",
+				}).Return(nil)
+				// 注册成功是 return nil
+				return usersvc
+			},
+
+			reqBody: `
+{
+	"email": "123@qq.com",
+	"password": "hello#world123",
+	"confirmPassword": "hello#world123"
+}
+`,
+			wantCode: http.StatusOK,
+			wantBody: "注册成功",
+		},
+		{
+			name: "参数不对，bind 失败",
+			mock: func(ctrl *gomock.Controller) service.UserService {
+				usersvc := svcmocks.NewMockUserService(ctrl)
+				// 注册成功是 return nil
+				return usersvc
+			},
+
+			reqBody: `
+{
+	"email": "123@qq.com",
+	"password": "hello#world123"
+`,
+			wantCode: http.StatusBadRequest,
+		},
+		{
+			name: "邮箱格式不对",
+			mock: func(ctrl *gomock.Controller) service.UserService {
+				usersvc := svcmocks.NewMockUserService(ctrl)
+				return usersvc
+			},
+
+			reqBody: `
+{
+	"email": "123@q",
+	"password": "hello#world123",
+	"confirmPassword": "hello#world123"
+}
+`,
+			wantCode: http.StatusOK,
+			wantBody: "你的邮箱格式不对",
+		},
+		{
+			name: "两次输入密码不匹配",
+			mock: func(ctrl *gomock.Controller) service.UserService {
+				usersvc := svcmocks.NewMockUserService(ctrl)
+				return usersvc
+			},
+
+			reqBody: `
+{
+	"email": "123@qq.com",
+	"password": "hello#world1234",
+	"confirmPassword": "hello#world123"
+}
+`,
+			wantCode: http.StatusOK,
+			wantBody: "两次输入的密码不一致",
+		},
+		{
+			name: "密码格式不对",
+			mock: func(ctrl *gomock.Controller) service.UserService {
+				usersvc := svcmocks.NewMockUserService(ctrl)
+				return usersvc
+			},
+			reqBody: `
+{
+	"email": "123@qq.com",
+	"password": "hello123",
+	"confirmPassword": "hello123"
+}
+`,
+			wantCode: http.StatusOK,
+			wantBody: "密码必须大于8位，包含数字、特殊字符",
+		},
+		{
+			name: "邮箱冲突",
+			mock: func(ctrl *gomock.Controller) service.UserService {
+				usersvc := svcmocks.NewMockUserService(ctrl)
+				usersvc.EXPECT().SignUp(gomock.Any(), domain.User{
+					Email:    "123@qq.com",
+					Password: "hello#world123",
+				}).Return(service.ErrUserDuplicateEmail)
+				// 注册成功是 return nil
+				return usersvc
+			},
+
+			reqBody: `
+{
+	"email": "123@qq.com",
+	"password": "hello#world123",
+	"confirmPassword": "hello#world123"
+}
+`,
+			wantCode: http.StatusOK,
+			wantBody: "邮箱冲突",
+		},
+		{
+			name: "系统异常",
+			mock: func(ctrl *gomock.Controller) service.UserService {
+				usersvc := svcmocks.NewMockUserService(ctrl)
+				usersvc.EXPECT().SignUp(gomock.Any(), domain.User{
+					Email:    "123@qq.com",
+					Password: "hello#world123",
+				}).Return(errors.New("随便一个 error"))
+				// 注册成功是 return nil
+				return usersvc
+			},
+
+			reqBody: `
+{
+	"email": "123@qq.com",
+	"password": "hello#world123",
+	"confirmPassword": "hello#world123"
+}
+`,
+			wantCode: http.StatusOK,
+			wantBody: "系统异常",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			server := gin.Default()
+			// 用不上 codeSvc
+			h := NewUserHandler(tc.mock(ctrl), nil)
+			h.RegisterRoutes(server)
+
+			req, err := http.NewRequest(http.MethodPost,
+				"/users/signup", bytes.NewBuffer([]byte(tc.reqBody)))
+			require.NoError(t, err)
+			// 数据是 JSON 格式
+			req.Header.Set("Content-Type", "application/json")
+			// 这里你就可以继续使用 req
+
+			resp := httptest.NewRecorder()
+			// 这就是 HTTP 请求进去 GIN 框架的入口。
+			// 当你这样调用的时候，GIN 就会处理这个请求
+			// 响应写回到 resp 里
+			server.ServeHTTP(resp, req)
+
+			assert.Equal(t, tc.wantCode, resp.Code)
+			assert.Equal(t, tc.wantBody, resp.Body.String())
+
+		})
+	}
 }
